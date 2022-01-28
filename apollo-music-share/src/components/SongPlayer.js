0284b40fdaf9def5e7ef5a25cbf844f4697a913d
allexib <ooomegasib@gmail.com>
2022-01-28 14:57:10 +0300

    songPlayer,play next music auto,useEffect nextSong

--- apollo-music-share/src/components/SongPlayer.js
+++ apollo-music-share/src/components/SongPlayer.js
@@ -46,8 +46,23 @@ function SongPlayer() {
     const reactPlayerRef = React.useRef()
     const {state, dispatch} = React.useContext(SongContext)
     const [played, setPlayed] = React.useState(0)
+    const [playedSeconds, setPlayedSeconds] = React.useState(0)
     const [seeking, setSeeking] = React.useState(false)
+    const [positionInQueue, setPositionInQueue] = React.useState(0)
     const classes = useStyles()
+
+    React.useEffect(() => {
+        const songIndex = data.queue.findIndex(song => song.id === state.song.id)
+        setPositionInQueue(songIndex)
+    }, [data.queue, state.song.id])
+
+    React.useEffect(() => {
+        const nextSong = data.queue[positionInQueue + 1]
+        if (played === 1 && nextSong) {
+            setPlayed(0)
+            dispatch({type: 'SET_SONG', payload: {song: nextSong}})
+        }
+    }, [data.queue, played, dispatch, positionInQueue])
 
     function handleTogglePlay() {
         dispatch(state.isPlaying ? {type: 'PAUSE_SONG'} : {type: 'PLAY_SONG'})
@@ -64,6 +79,10 @@ function SongPlayer() {
     function handleSeekMouseUp() {
         setSeeking(false)
         reactPlayerRef.current.seekTo(played)
+    }
+
+    function formatDuration(seconds) {
+        return new Date(seconds * 1000).toISOString().substr(11, 8)
     }
 
     return (
@@ -90,7 +109,7 @@ function SongPlayer() {
                             <SkipNextIcon/>
                         </IconButton>
                         <Typography variant='subtitle1' component='p' color='textSecondary'>
-                            00:01:22
+                            {formatDuration(playedSeconds)}
                         </Typography>
                     </div>
                     <Slider
@@ -109,6 +128,7 @@ function SongPlayer() {
                     onProgress={({played, playedSeconds}) => {
                         if (!seeking) {
                             setPlayed(played)
+                            setPlayedSeconds(playedSeconds)
                         }
                     }}
                     url={state.song.url} playing={state.isPlaying} hidden/>
